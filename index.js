38df4ed9c8f9ca48ab9b137b47ce23f93941a492
Yannick Severo <severoyannick@gmail.com>
2021-10-05 12:14:04 +0200

    Étape 3-4-5

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 // Création d'un serveur express
-const express = require("express");
-const app = express();
+const express = require('express');
+const articles = require('./data/articles.json');
+
 const PORT = 3000;
 
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/app/views");
+const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", {
-    root: __dirname + "/integration/index.html",
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/app/views');
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {  
+  res.render('list', {
+    listOfArticles: articles,
+  });
+});
+
+app.get('/article/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const foundArticle = articles.find((testedArticle) => {
+    return testedArticle.id === id;
+  });
+  res.render('article', {
+    article: foundArticle,
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+  console.log(`Server listening on ${PORT}`);
+}); 
