d14db3bcfa89c2805eb4ec98050ae10726c8b44b
Yannick Severo <severoyannick@gmail.com>
2021-10-05 08:59:02 +0200

    Étape 2

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/app/views");
 
+app.get("/", (req, res) => {
+  res.sendFile("index.html", {
+    root: __dirname + "/integration/index.html",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
